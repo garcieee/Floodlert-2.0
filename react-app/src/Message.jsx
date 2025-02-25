9d595bbb836965c71103142dd2e766902981851a
mozes-lol <johnmarkgarcia164@gmail.com>
2025-02-25 14:58:46 +0800

    Testing out react

---
+++ react-app/src/Message.jsx
@@ -0,0 +1,10 @@
+function Message() {
+    const name = 'Love';
+    if (name) {
+        return <h1>{name} you nigger</h1>;        
+    } else {
+        return <h1>Fuck you nigger</h1>;
+    }
+}
+
+export default Message;
